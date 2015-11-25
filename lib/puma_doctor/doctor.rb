6405d502f4119704df9c3a0bb4f7692893aa6b63
Alex Krasynskyi <lyoshakr@gmail.com>
2015-11-25 17:29:57 +0200

    fixed killing worker

--- lib/puma_doctor/doctor.rb
+++ lib/puma_doctor/doctor.rb
@@ -16,7 +16,7 @@ module PumaDoctor
       used_memory = workers.inject(0) {|memo, v| memo += v.last } + GetProcessMem.new(@master_pid).mb
       logger.info "[Puma Doctor] Total memory used: #{used_memory} mb. Workers online: #{workers.size}"
       if used_memory > @memory_threshold
-        kill_worker(workers.last.first)
+        kill_worker(workers)
         # decrease_workers_amount
       else
         refresh_stats
@@ -51,11 +51,12 @@ module PumaDoctor
       end.sort {|a, b| a[1] <=> b[1]}
     end
 
-    def kill_worker(pid, workers = [])
+    def kill_worker(workers)
       # @conseq_skips = 0
       # @conseq_kills += 1
+      pid = workers.last.first
       Process.kill('TERM', pid)
-      logger.info "[Puma Doctor] Doctor killed worker(#{workers.last.first}).It was using #{workers.last.last} mb. Workers online: #{workers.size}"
+      logger.info "[Puma Doctor] Doctor killed worker(#{pid}).It was using #{workers.last.last} mb. Workers online: #{workers.size}"
     end
 
     def refresh_stats
